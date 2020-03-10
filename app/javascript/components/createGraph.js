fafa98abd55095a719949703b9757d86eab5328d
efedra <efedra-00@mail.ru>
2020-03-10 19:42:12 +0300

    Repair function CreateGraph

--- app/javascript/components/createGraph.js
+++ app/javascript/components/createGraph.js
@@ -3,10 +3,27 @@ import * as d3 from "d3";
 
 class CreateGraph extends Component {
 
+    constructor(props) {
+        super(props)
+        // set up SVG for D3
+        this.state = {
+            width: window.innerWidth,
+            height: window.innerHeight,
+            radiusNode: 20,
+        }
+    }
+    create()
+    {
+        d3.select("svg").remove()
+    }
     render() {
+    return(
+        <div>
+            {this.create()}
+        </div>
+    )
 
-        return null;
     }
 }
 
-export default CreateGraph
+export default CreateGraph;
