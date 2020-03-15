d290374018effa649bba28024da45c2df22eceb5
tevvi <antoxa.com.com@mail.ru>
2020-03-15 12:18:44 +0300

    setup websockets

---
+++ app/channels/graphs_channel.rb
@@ -0,0 +1,13 @@
+class GraphsChannel < ApplicationCable::Channel
+  def subscribed
+    stream_from current_graph
+  end
+
+  def unsubscribed
+    # Any cleanup needed when channel is unsubscribed
+  end
+
+  def current_graph
+    @current_graph ||= current_user.graphs.find(params[:id])
+  end
+end
--- app/models/graph.rb
+++ app/models/graph.rb
@@ -2,9 +2,12 @@
 
 class Graph < ApplicationRecord
   has_many :nodes, dependent: :nullify
-  belongs_to :user, dependent: :nullify
+  belongs_to :user
 
   validates :name, :state, presence: true
+
+  after_update_commit { GraphBroadcastJob.perform_later self, 'graph_update', self.as_json }
+  after_destroy { GraphBroadcastJob.perform_later self, 'graph_destroy' }
 
   def edges
     Edge.where(start_id: nodes.select(:id))
