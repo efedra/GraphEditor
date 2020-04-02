d76e03725baebc14a0e2c0a2efa6c4a80b4c8bce
efedra <efedra-00@mail.ru>
2020-04-02 22:17:11 +0300

    Add input for colors of node

--- app/javascript/components/Editor.js
+++ app/javascript/components/Editor.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-export default class Editor extends React.Component{
+export default class Editor extends React.Component {
 
     constructor(props) {
         super(props);
@@ -8,45 +8,56 @@ export default class Editor extends React.Component{
         this.handleChange = this.handleChange.bind(this);
 
     }
+
     handleChange(event) {
         this.props.onChange(event.type, parseInt(event.elementId), event.data);
     }
 
-    handleXChange(e){
-        this.handleChange({type: this.props.element.elementType, elementId: this.props.element.elementId,
-            data: {x: parseInt(e.target.value)}})
+    handleXChange(e) {
+        this.handleChange({
+            type: this.props.element.elementType, elementId: this.props.element.elementId,
+            data: {x: parseInt(e.target.value)}
+        })
     }
-    handleYChange(e){
-        this.handleChange({type: this.props.element.elementType, elementId: this.props.element.elementId,
-            data: {y: parseInt(e.target.value)}})
+
+    handleYChange(e) {
+        this.handleChange({
+            type: this.props.element.elementType, elementId: this.props.element.elementId,
+            data: {y: parseInt(e.target.value)}
+        })
     }
-    render()   {
-         function buildFieldSet(element, owner){
-             if (element.data !== undefined)
-             {
-                 return <fieldset>
-                     <legend>X</legend>
-                     <input type="number"
-                            value={element.data.x}
-                            onChange={owner.handleXChange.bind(owner)} />
-                     <legend>Y</legend>
-                     <input type="number"
-                            value={element.data.y}
-                            onChange={owner.handleYChange.bind(owner)} />
-                 </fieldset>
-             }
+    handleColorChange(e){
+        this.handleChange({
+            type: this.props.element.elementType, elementId: this.props.element.elementId,
+            data: {color: e.target.value}
+        })
+    }
+    render() {
+        function buildFieldSet(element, owner) {
+            if (element.data !== undefined) {
+                return <fieldset>
+                    <legend>X</legend>
+                    <input type="number"
+                           value={element.data.y}
+                           onChange={owner.handleXChange.bind(owner)}/>
+                    <legend>Y</legend>
+                    <input type="number"
+                           value={element.data.y}
+                           onChange={owner.handleYChange.bind(owner)}/>
+                    <legend>Color</legend>
+                    <input type="text" value={ element.data.color !=null?element.data.color:""} onChange={owner.handleColorChange.bind(owner)} />
+                </fieldset>
+            }
 
         }
-        if (this.props.element != null)
-        {
+
+        if (this.props.element != null) {
             return <div className='Editor'>
                 <div>{this.props.element.elementType} {this.props.element.elementId}</div>
                 {buildFieldSet(this.props.element, this)}
 
             </div>;
-        }
-        else
-        {
+        } else {
             return <div></div>;
         }
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_06_182640) do
+ActiveRecord::Schema.define(version: 2020_03_13_080545) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,8 @@ ActiveRecord::Schema.define(version: 2020_02_06_182640) do
     t.jsonb "state", default: "{}", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_graphs_on_user_id"
   end
 
   create_table "nodes", force: :cascade do |t|
@@ -42,5 +44,18 @@ ActiveRecord::Schema.define(version: 2020_02_06_182640) do
     t.index ["graph_id"], name: "index_nodes_on_graph_id"
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  add_foreign_key "graphs", "users"
   add_foreign_key "nodes", "graphs"
 end
