19a4797d1fcf557bd76cf6b1d39a847f608573a3
tevvi <antoxa.com.com@mail.ru>
2020-04-07 23:42:12 +0300

    add validation to :kind enum

--- app/models/node.rb
+++ app/models/node.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class Node < ApplicationRecord
+  include LiberalEnum
+
   belongs_to :graph
   has_many :output_edges,
     foreign_key: :start_id,
@@ -13,9 +15,11 @@ class Node < ApplicationRecord
     dependent: :nullify,
     inverse_of: :finish
 
-  enum kind: { start: 0, intermediate: 1, finish: 2, invalid: 3 }
+  enum kind: { start: 0, intermediate: 1, finish: 2, bad: 3 }
+  liberal_enum :kind
 
-  validates :html_x, :html_y, presence: true
+  validates :kind, presence: true, inclusion: { in: Node.kinds.keys }
+  validates :html_x, :html_y, presence: true, numericality: { only_integer: true }
 
   after_create_commit { GraphBroadcastJob.perform_later graph, 'node_create', as_json }
   after_update_commit { GraphBroadcastJob.perform_later graph, 'node_update', as_json }
