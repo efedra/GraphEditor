b8920cd6e9d0cea8a4fa7021103a05079380700d
Innominat0 <innominato@rambler.ru>
2020-04-22 13:51:04 +0300

    path from a to b
    no weights, just path

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,11 +8,17 @@ class HomeController < ApplicationController
 
   def test_graph
     nodes = [1, 2, 3, 4, 5]
-    edges = [[1, 2], [1, 3], [2, 3], [4, 5]]
+    edges = [[1, 2], [1, 3], [2, 3], [3, 4], [4, 5]]
 
     gm = GraphManager.new
     n, e, c = gm.count_components(nodes, edges)
-    render json: {components: c, node_count: n, edge_count: e}
+    #render json: { components: c, node_count: n, edge_count: e }
+
+    nodes1 = [1, 2, 3, 4, 5, 6, 7]
+    edges1 = [[1, 2], [1, 4], [2, 3], [3, 4],[ 4, 5],  [5, 6], [5, 7]]
+
+       n1, e1, p = gm.find_path(nodes1, edges1, 1, 7)
+       render json: { path: p, node_count: n1, edge_count: e1 }
   end
 end
 
--- app/services/graph_manager.rb
+++ app/services/graph_manager.rb
@@ -1,5 +1,6 @@
 require 'rgl/adjacency'
 require 'rgl/connected_components'
+require 'rgl/traversal'
 
 
 class GraphManager
@@ -19,6 +20,46 @@ class GraphManager
 
     return dg.vertices.count, dg.edges.count, a
 
+  end
+
+  # no weights, not shortest by weight, just path from a to b
+  def find_path (nodes, edges,  node1, node2)
+    dg = RGL::DirectedAdjacencyGraph.new()
+    nodes.each { |n| dg.add_vertex n }
+    edges.each { |e| dg.add_edge e[0], e[1] }
+
+    path = []
+
+    if node1 == node2
+      path << node1
+    else # different nodes
+      tree = dg.bfs_search_tree_from(node1)
+
+      tree = tree.reverse
+
+      tmpv = node2
+
+      # if there is no path node2 will not be present in the tree
+      # if no path return nil
+      if tree.has_vertex?(node2) == false
+        path = nil
+        return dg.vertices.count, dg.edges.count, path
+
+      end
+
+      until tmpv == node1 do
+        path << tmpv
+        tmpv = tree.adjacent_vertices(tmpv)[0]
+
+      end
+
+      path << tmpv
+
+    end
+
+    # reverse path?
+
+    return dg.vertices.count, dg.edges.count, path
   end
 
 end
